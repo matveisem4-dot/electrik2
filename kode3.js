c47cf240c00549c5395f7558823d35bb18422c0a
matveisem4-dot <matveisem4@gmail.com>
2026-02-19 16:52:06 +0200

    Update kode3.js

--- kode3.js
+++ kode3.js
@@ -1,23 +1,6 @@
-// SYNAPSE ZERO Neural Core
-class SynapseNet {
-    constructor() {
-        this.w = [Math.random(), Math.random()];
-        this.b = Math.random();
-        this.rate = 0.05;
+export const KnowledgeCore = {
+    process: (text) => {
+        const words = text.trim().split(/\s+/).length;
+        return { wordCount: words, status: "СИНХРОНИЗИРОВАНО" };
     }
-
-    activate(x) { return 1 / (1 + Math.exp(-x)); }
-
-    think(inputs) {
-        let sum = this.b;
-        inputs.forEach((val, i) => sum += val * this.w[i]);
-        return this.activate(sum);
-    }
-
-    learn(inputs, target) {
-        let guess = this.think(inputs);
-        let err = target - guess;
-        this.w = this.w.map((w, i) => w + err * inputs[i] * this.rate);
-        this.b += err * this.rate;
-    }
-}
+};
